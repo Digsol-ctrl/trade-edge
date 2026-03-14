2d05ebb3654d089ccbb1acef5b30ce28955386be
tendai <mufambisitendaiblessed@gmail.com>
2026-03-14 12:10:22 +0200

    commented out some trade routes that are not being used yet.

--- server/src/routes/tradeRoutes.js
+++ server/src/routes/tradeRoutes.js
@@ -1,18 +1,19 @@
 import express from 'express';
-import { getTrades,
+/* import { getTrades,
     getTradeById,
      createTrade,
      updateTrade,
      deleteTrade
- } from '../controllers/tradeController.js';
+ } from '../controllers/tradeController.js'; */
+import { createTrade } from '../controllers/tradeController.js'
 import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', protect, getTrades);
+//router.get('/', protect, getTrades);
 router.post('/', protect, createTrade);
-router.put('/:id', protect, updateTrade);
-router.delete('/:id', protect, deleteTrade);
-router.get('/:id', protect, getTradeById);
+//router.put('/:id', protect, updateTrade);
+//router.delete('/:id', protect, deleteTrade);
+//router.get('/:id', protect, getTradeById);
 
 export default router;
